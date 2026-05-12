2754515aa5915431f970b6018f381b92b1955ec8
yuans-dev <u1benedict425@gmail.com>
2026-05-12 23:33:23 +0800

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -75,5 +75,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|assets).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|assets).*)"],
 };
